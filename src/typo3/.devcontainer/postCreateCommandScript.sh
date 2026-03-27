a2671aac0a1eb67a863948dd1864f7058b9d865f
Thomas Hucke <github@thucke.de>
2026-03-27 18:01:02 +0000

    fix: setting correct COMPOSE_PROFILES

--- src/typo3/.devcontainer/postCreateCommandScript.sh
+++ src/typo3/.devcontainer/postCreateCommandScript.sh
@@ -9,9 +9,10 @@ if [ -n `which docker` ] && [ "${TYPO3_INSTALL_DB_DRIVER:-pdo_sqlite}" != "pdo_s
   echo "postCreateCommandScript: Checking if docker containers are already running"
   if [ `docker compose ls -q --filter "name=^${COMPOSE_PROJECT_NAME}$" | wc -l` -eq 0 ]; then
     compose_filename="${WORKSPACE_ROOT}/.devcontainer/docker/docker-compose.backend.yaml"
+    export COMPOSE_PROFILES+=", ${DB_SERVER_TYPE}"
     # start docker containers for initialization
-    echo "postCreateCommandScript: Starting docker containers for ${COMPOSE_PROJECT_NAME}"
-    docker compose -f ${compose_filename} up -d --wait
+    echo "postCreateCommandScript: Starting docker containers for ${COMPOSE_PROJECT_NAME} with database server type ${DB_SERVER_TYPE}"
+    docker compose -f ${compose_filename} up -d --wait || [ $? -eq 1 ] && echo "No docker containers are currently needed. Maybe TYPO3_INSTALL_DB_DRIVER is set to 'pdo_sqlite' in '.devcontainer/.env'."
   else
     echo "Docker containers for ${COMPOSE_PROJECT_NAME} are already running."
   fi
