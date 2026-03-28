f6e43fef750f6d67d6a70add379b91a1863a425d
Thomas Hucke <github@thucke.de>
2026-03-28 17:46:36 +0000

    feat: choose database type via template

--- src/typo3/.devcontainer/docker/igniteEnvironment.sh
+++ src/typo3/.devcontainer/docker/igniteEnvironment.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
-set -u
-
+# 
+# !!!! IMPORTANT: WORKSPACE_ROOT has to be set before sourcing this script
 echo "BEGIN: igniteEnvironment.sh"
 
-script=$(readlink -f "$0")
-scriptdir=$(dirname "$script")
-
-source $scriptdir/parseDotEnv.sh
-
+set -u
 pushd ${WORKSPACE_ROOT}
 
-echo "Step 1/6: Reset environment"
-rm -rf config
-rm -rf .build/bin
-rm -rf .build/public
-rm -rf .build/vendor
-rm -rf var
+source .devcontainer/docker/parseDotEnv.sh
 
-mkdir -vp .build/public
-mkdir -vp var/log/
-
+echo "initializeTYPO3.sh: Reset environment"
+rm -rfv config .build/bin .build/public .build/vendor var
+mkdir -vp .build/public var/log/ var/lib/ $(dirname ${TYPO3_INSTALL_DB_DBNAME})
 
 if [ "${TYPO3_INSTALL_DB_DRIVER}" == "mysqli" ]; then
   echo "Using MySQL/MariaDB as database - resetting database"
@@ -29,38 +20,41 @@ if [ "${TYPO3_INSTALL_DB_DRIVER}" == "mysqli" ]; then
       mysql -h${TYPO3_INSTALL_DB_HOST} -P${TYPO3_INSTALL_DB_PORT} -u${TYPO3_INSTALL_DB_USER} -p${TYPO3_INSTALL_DB_PASSWORD} -v ${TYPO3_INSTALL_DB_DBNAME}
 
     mysql -h${TYPO3_INSTALL_DB_HOST} -P${TYPO3_INSTALL_DB_PORT} -u${TYPO3_INSTALL_DB_USER} -p${TYPO3_INSTALL_DB_PASSWORD} --init-command='USE '${TYPO3_INSTALL_DB_DBNAME} < .devcontainer/docker/db/initdb/2_create_procedure.sql
+elif [ "${TYPO3_INSTALL_DB_DRIVER}" == "pdo_sqlite" ] && [ -f ${TYPO3_INSTALL_DB_DBNAME} ]; then
+  echo "Using SQLite as database - resetting database"
+  rm -fv ${TYPO3_INSTALL_DB_DBNAME}
 fi
 
 if [ -f ${WORKSPACE_ROOT}/composer.json ]; then
-  echo "Step 2/6: initializeTYPO3.sh"
+  echo "initializeTYPO3.sh: Found composer.json - running initializeTYPO3.sh"
   chmod +x .devcontainer/docker/initializeTYPO3.sh
   .devcontainer/docker/initializeTYPO3.sh
 
-  echo "Step 3/6: Composer up"
+  echo "initializeTYPO3.sh: Running composer up"
   composer up
 
   if [ ! -f config/system/additional.php ]; then
-    echo "Step 4/6: Add additional.php if not exists"
+    echo "initializeTYPO3.sh: Add additional.php if not exists"
     cp .devcontainer/docker/typo3/additional.php config/system/additional.php
   else
-    echo "Step 4/6: additional.php already exists - skipping"
+    echo "initializeTYPO3.sh: additional.php already exists - skipping"
   fi
 
   if [ "${TYPO3_INSTALL_DB_DRIVER}" == "mysqli" ]; then
-    echo "Step 5/6: Fix TYPO3 image references"
+    echo "initializeTYPO3.sh: Fix TYPO3 image references"
     mysql -h127.0.0.1 -P3306 -u${TYPO3_INSTALL_DB_USER} -p${TYPO3_INSTALL_DB_PASSWORD} -e 'use '${TYPO3_INSTALL_DB_DBNAME}'; call fixImgInTtContent();'
   else
-    echo "Step 5/6: Fix TYPO3 image references - skipping as database driver is not mysqli"
+    echo "initializeTYPO3.sh: Fix TYPO3 image references - skipping as database driver is not mysqli"
   fi
 
   if [ "${COMPOSE_PROFILES}" == "php-fpm" ]; then
-    echo "Step 6/6: Checking for running php-fpm"
+    echo "initializeTYPO3.sh: Checking for running php-fpm"
     .devcontainer/php-fpm/server.sh restart
   elif [ "${COMPOSE_PROFILES}" == "apache" ]; then
-    echo "Step 6/6: Restarting Apache"
+    echo "initializeTYPO3.sh: Restarting Apache"
     .devcontainer/php-fpm/server.sh
   elif [ "${COMPOSE_PROFILES}" == "frankenphp" ]; then
-    echo "Step 6/6: Restarting FrankenPHP"
+    echo "initializeTYPO3.sh: Restarting FrankenPHP"
     .devcontainer/docker/frankenphp/server.sh restart
   fi
 
