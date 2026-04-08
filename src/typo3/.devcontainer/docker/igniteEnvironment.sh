ea4d4ea8bc1c19769fd40b6f42ebd38b54ba58a0
Thomas Hucke <github@thucke.de>
2026-04-08 18:31:56 +0200

    chore: hopefully last fixes for test with introduction extension
    * feat: initialize typo3 template
    
    * chore: added file
    
    * feat: added typo3 template
    
    * chore: fix tests & remove color template
    
    * chore: force ci in for typo3
    
    * chore: test login to registry before
    
    * chore: added env in workflow
    
    * chore: reactivated GITHUB_TOKEN in workflow
    
    * chore: cleanup test additions in action
    
    * chore: fixed tests for typo3
    
    * chore: fixed tests for typo3
    
    * fix: adjusted db container timing
    
    * docs: refactor begin
    
    * docs: NOTES.md
    
    * docs: NOTES.md
    
    * chore: raised typo3 template version
    
    * fix: conditionals in postCreateCommandscript.sh and others
    
    * fix: other conditionals
    
    * fix: setting correct COMPOSE_PROFILES
    
    * fix: no permission on test cleanup
    
    * feat: choose database type via template
    
    * chore: finalize for 0.3.0
    
    * docs: update some doc info
    
    * chore: fix scripts
    
    * chore: add TYPO3.env to .gitignore
    
    * chore: fix parseDotEnv.sh
    
    * chore: fix TYPO3 initialization
    
    * chore: template 2be validated with introduction extension
    
    * chore: hopefully last fixes for test with introduction extension
    
    * chore: wtf - another file
    
    * chore: wtf - another fix
    
    * chore: formatting fix

--- src/typo3/.devcontainer/docker/igniteEnvironment.sh
+++ src/typo3/.devcontainer/docker/igniteEnvironment.sh
@@ -3,11 +3,10 @@
 # !!!! IMPORTANT: WORKSPACE_ROOT has to be set before sourcing this script
 echo "BEGIN: igniteEnvironment.sh"
 
-typo3_major_version=$(typo3 -V|grep "TYPO3 CMS"|perl -n -e '/\ ([\d]+)/ && print $1')
-echo "initializeTYPO3: Running on TYPO3 major version ${typo3_major_version}"
+set -u
+pushd ${WORKSPACE_ROOT}
 
-echo "initializeTYPO3: DB compare"
-typo3 database:updateschema --no-interaction --no-ansi || true
+source .devcontainer/docker/parseDotEnv.sh
 
 echo "igniteEnvironment.sh: Reset environment"
 rm -rfv config .build/bin .build/public .build/vendor var
@@ -19,17 +18,21 @@ else
   echo "igniteEnvironment.sh: No need to create SqLite datafile directory"
 fi
 
-#echo "initializeTYPO3: Create new backend user"
-#$typo3_cli backend:createadmin ${TYPO3_INSTALL_ADMIN_USER} ${TYPO3_INSTALL_ADMIN_PASSWORD} --no-interaction --no-ansi || true
+if [ "${TYPO3_INSTALL_DB_DRIVER}" == "mysqli" ]; then
+  echo "Using MySQL/MariaDB as database - resetting database"
+  # Drop all database tables
+  mysql -h${TYPO3_INSTALL_DB_HOST} -P${TYPO3_INSTALL_DB_PORT} -u${TYPO3_INSTALL_DB_USER} -p${TYPO3_INSTALL_DB_PASSWORD} --silent --skip-column-names -e "SHOW TABLES" ${TYPO3_INSTALL_DB_DBNAME} | \
+      xargs -I% echo 'SET FOREIGN_KEY_CHECKS = 0; DROP TABLE %; SET FOREIGN_KEY_CHECKS = 1;' | \
+      mysql -h${TYPO3_INSTALL_DB_HOST} -P${TYPO3_INSTALL_DB_PORT} -u${TYPO3_INSTALL_DB_USER} -p${TYPO3_INSTALL_DB_PASSWORD} -v ${TYPO3_INSTALL_DB_DBNAME}
 
-    mysql -h${TYPO3_INSTALL_DB_HOST} -P${TYPO3_INSTALL_DB_PORT} -u${TYPO3_INSTALL_DB_USER} -p${TYPO3_INSTALL_DB_PASSWORD} --init-command='USE '${TYPO3_INSTALL_DB_DBNAME} < .devcontainer/docker/db/initdb/2_create_procedure.sql
+  mysql -h${TYPO3_INSTALL_DB_HOST} -P${TYPO3_INSTALL_DB_PORT} -u${TYPO3_INSTALL_DB_USER} -p${TYPO3_INSTALL_DB_PASSWORD} --init-command='USE '${TYPO3_INSTALL_DB_DBNAME} < .devcontainer/docker/db/initdb/2_create_procedure.sql
 elif [ "${TYPO3_INSTALL_DB_DRIVER}" == "pdo_sqlite" ] && [ -f ${SQLITE_DBFILE_PATH} ]; then
   echo "Using SQLite as database - resetting database"
   rm -fv ${SQLITE_DBFILE_PATH}
 fi
 
 if [ -f ${WORKSPACE_ROOT}/composer.json ]; then
-  echo "initializeTYPO3.sh: Found composer.json - running initializeTYPO3.sh"
+  echo "igniteEnvironment.sh: Found composer.json - running initializeTYPO3.sh"
   chmod +x .devcontainer/docker/initializeTYPO3.sh
   .devcontainer/docker/initializeTYPO3.sh
 else
@@ -37,14 +40,15 @@ else
 fi
 
 if [[ "${COMPOSE_PROFILES}" =~ "php-fpm" ]]; then
-    echo "initializeTYPO3.sh: Checking for running php-fpm"
+    echo "igniteEnvironment.sh: Checking for running php-fpm"
     .devcontainer/php-fpm/server.sh restart
 elif [[ "${COMPOSE_PROFILES}" =~ "apache" ]]; then
-    echo "initializeTYPO3.sh: Restarting Apache"
+    echo "igniteEnvironment.sh: Restarting Apache"
     .devcontainer/php-fpm/server.sh
 elif [[ "${COMPOSE_PROFILES}" =~ "frankenphp" ]]; then
-    echo "initializeTYPO3.sh: Restarting FrankenPHP"
+    echo "igniteEnvironment.sh: Restarting FrankenPHP"
     .devcontainer/docker/frankenphp/server.sh restart
 fi
 
-echo "END: initializeTYPO3.sh"
+echo "END: igniteEnvironment.sh"
+popd
