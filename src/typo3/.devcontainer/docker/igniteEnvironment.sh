39fef6e7c2784c69d8ae7a1fa6daddb014ecfe71
Thomas HUCKE <github@thucke.de>
2026-04-08 18:14:33 +0200

    chore: wtf - another file

--- src/typo3/.devcontainer/docker/igniteEnvironment.sh
+++ src/typo3/.devcontainer/docker/igniteEnvironment.sh
@@ -3,11 +3,10 @@
 # !!!! IMPORTANT: WORKSPACE_ROOT has to be set before sourcing this script
 echo "BEGIN: igniteEnvironment.sh"
 
-typo3_major_version=$(typo3 -V|grep "TYPO3 CMS"|perl -n -e '/\ ([\d]+)/ && print $1')
-echo "initializeTYPO3: Running on TYPO3 major version ${typo3_major_version}"
+set -u
+pushd ${WORKSPACE_ROOT}
 
-echo "initializeTYPO3: DB compare"
-typo3 database:updateschema --no-interaction --no-ansi || true
+source .devcontainer/docker/parseDotEnv.sh
 
 echo "igniteEnvironment.sh: Reset environment"
 rm -rfv config .build/bin .build/public .build/vendor var
@@ -19,8 +18,12 @@ else
   echo "igniteEnvironment.sh: No need to create SqLite datafile directory"
 fi
 
-#echo "initializeTYPO3: Create new backend user"
-#$typo3_cli backend:createadmin ${TYPO3_INSTALL_ADMIN_USER} ${TYPO3_INSTALL_ADMIN_PASSWORD} --no-interaction --no-ansi || true
+if [ "${TYPO3_INSTALL_DB_DRIVER}" == "mysqli" ]; then
+  echo "Using MySQL/MariaDB as database - resetting database"
+  # Drop all database tables
+  mysql -h${TYPO3_INSTALL_DB_HOST} -P${TYPO3_INSTALL_DB_PORT} -u${TYPO3_INSTALL_DB_USER} -p${TYPO3_INSTALL_DB_PASSWORD} --silent --skip-column-names -e "SHOW TABLES" ${TYPO3_INSTALL_DB_DBNAME} | \
+      xargs -I% echo 'SET FOREIGN_KEY_CHECKS = 0; DROP TABLE %; SET FOREIGN_KEY_CHECKS = 1;' | \
+      mysql -h${TYPO3_INSTALL_DB_HOST} -P${TYPO3_INSTALL_DB_PORT} -u${TYPO3_INSTALL_DB_USER} -p${TYPO3_INSTALL_DB_PASSWORD} -v ${TYPO3_INSTALL_DB_DBNAME}
 
     mysql -h${TYPO3_INSTALL_DB_HOST} -P${TYPO3_INSTALL_DB_PORT} -u${TYPO3_INSTALL_DB_USER} -p${TYPO3_INSTALL_DB_PASSWORD} --init-command='USE '${TYPO3_INSTALL_DB_DBNAME} < .devcontainer/docker/db/initdb/2_create_procedure.sql
 elif [ "${TYPO3_INSTALL_DB_DRIVER}" == "pdo_sqlite" ] && [ -f ${SQLITE_DBFILE_PATH} ]; then
@@ -47,4 +50,5 @@ elif [[ "${COMPOSE_PROFILES}" =~ "frankenphp" ]]; then
     .devcontainer/docker/frankenphp/server.sh restart
 fi
 
-echo "END: initializeTYPO3.sh"
+echo "END: igniteEnvironment.sh"
+popd
